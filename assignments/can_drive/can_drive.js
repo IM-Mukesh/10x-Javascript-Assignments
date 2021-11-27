301c08acf40a741daa2ddd1dcf3ff544e8c1e182
IM-Mukesh <muke@gmail.com>
2021-11-27 16:51:14 +0530

    All Test cases passed

--- assignments/can_drive/can_drive.js
+++ assignments/can_drive/can_drive.js
@@ -1,5 +1,12 @@
 function CanDrive(hasDrivingLiscence, isTired, isSober) {
     // Write you code here
+    if(hasDrivingLiscence === false && isTired === true &&  isSober === true){
+        return "You cannot drive"
+    }else if(hasDrivingLiscence === true && isTired === false &&  isSober === true){
+        return "You can drive"
+    }else{
+        return "You shouldn't drive"
+    }
 }
 
 module.exports = CanDrive;
